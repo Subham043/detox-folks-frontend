d88b058d75193ca00ceed9fcafde7149df05b5aa
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-10-29 11:42:52 +0530

    completed special pages

--- src/components/SpecialProducts.tsx
+++ src/components/SpecialProducts.tsx
@@ -3,40 +3,79 @@ import useSWR from 'swr'
 import { api_routes } from "@/helper/routes";
 import { ProductResponseType } from "@/helper/types";
 import ProductCard from '@/components/ProductCard';
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 const loadingArr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-export default function SpecialProducts({title="On Sale Products", filter="is_on_sale"}:{title?:string, filter?:"is_on_sale"|"is_new"|"is_featured"}) {
-    const { data, isLoading } = useSWR<ProductResponseType>(api_routes.products + `?total=12&page=1&sort=id&filter[${filter}]=true&filter[is_random]=true`);
+export default function SpecialProducts({ title = "On Sale Products", filter = "is_on_sale", displayFilter = false }: { title?: string, filter?: "is_on_sale" | "is_new" | "is_featured", displayFilter?: boolean }) {
+  const [sort, setSort] = useState('id')
+  const [total, setTotal] = useState("12")
+  const [page, setPage] = useState("1")
+  const { data, isLoading } = useSWR<ProductResponseType>(api_routes.products + `?total=${total}&page=${page}&sort=${sort}&filter[${filter}]=true&filter[is_random]=true`);
 
-    return <section className="section feature-part">
+  return <section className="section feature-part">
     <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="section-heading"><h2>{title}</h2></div>
-        </div>
-      </div>
-      {isLoading && <div className="row">
-            {
-                loadingArr.map( i => <div className="col-sm-12 col-xl-3 col-lg-3 col-md-4 mb-2" key={i}>
-                    <div className="product-img-loading"></div>
-            </div>)
-            }
-        </div>}
-      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-4">
-        {
-            !isLoading && data?.data.map((item, i)=><ProductCard key={i} {...item} />)
-        }
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="section-btn-25">
-            <Link href="/products" className="btn btn-outline"
-              ><i className="fas fa-eye"></i><span>show more</span></Link
-            >
+      {
+        !displayFilter && 
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="section-heading"><h2>{title}</h2></div>
           </div>
         </div>
+      }
+      {
+        displayFilter &&
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="top-filter">
+                    <div className="filter-show">
+                        <label className="filter-label">Show :</label
+                        ><select className="form-select filter-select" value={total} onChange={(e) => setTotal(e.target.value)}>
+                            <option value="12">12</option>
+                            <option value="20">20</option>
+                            <option value="60">60</option>
+                            <option value="100">100</option>
+                        </select>
+                    </div>
+                    <div className="filter-short">
+                        <label className="filter-label">Sort by :</label
+                        ><select className="form-select filter-select" value={sort} onChange={(e) => setSort(e.target.value)}>
+                            <option value="-id">Latest</option>
+                            <option value="id">Oldest</option>
+                            <option value="name">A-Z</option>
+                            <option value="-name">Z-A</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+      }
+      {isLoading && <div className="row">
+        {
+          loadingArr.map(i => <div className="col-sm-12 col-xl-3 col-lg-3 col-md-4 mb-2" key={i}>
+            <div className="product-img-loading"></div>
+          </div>)
+        }
+      </div>}
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-4 row-cols-xl-4 justify-content-center">
+        {
+          !isLoading && data?.data.map((item, i) => <ProductCard key={i} {...item} />)
+        }
       </div>
+      {
+          displayFilter ? 
+          <Pagination {...data?.meta} paginationHandler={setPage} /> : 
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-btn-25">
+                <Link href={`/special-products/${filter}`} className="btn btn-outline"
+                ><i className="fas fa-eye"></i><span>show more</span></Link
+                >
+              </div>
+            </div>
+          </div>
+      }
     </div>
   </section>;
 }
---
+++ src/pages/special-products/[slug].tsx
@@ -0,0 +1,74 @@
+import Head from 'next/head'
+import Hero from '@/components/Hero';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import SpecialProducts from '@/components/SpecialProducts';
+
+type ServerSideProps = {
+    data: {
+        title: string;
+        slug: "is_featured" | "is_new" | "is_on_sale" | undefined;
+    };
+}
+
+export const getServerSideProps: GetServerSideProps<{
+    repo: ServerSideProps
+}> = async (ctx: any) => {
+    switch (ctx?.params.slug) {
+        case 'is_featured':
+            return {
+                props: {
+                    repo: {
+                        data: {
+                            title: "Our Featured Products",
+                            slug: "is_featured",
+                        },
+                    }
+                }
+            }
+        case 'is_new':
+            return {
+                props: {
+                    repo: {
+                        data: {
+                            title: "Our New Products",
+                            slug: "is_new",
+                        },
+                    }
+                }
+            }
+        case 'is_on_sale':
+            return {
+                props: {
+                    repo: {
+                        data: {
+                            title: "On Sale Products",
+                            slug: "is_on_sale",
+                        },
+                    }
+                }
+            }
+    
+        default:
+            return {
+                notFound: true,
+            }
+    }
+
+}
+
+export default function SpecialProductPage({
+    repo,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+    return <>
+            <Head>
+                <title>DetoxFolks - {repo.data.title}</title>
+                <meta name="description" content={repo.data.title} />
+                <meta name="keywords" content={repo.data.title} />
+                <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+                <link rel="icon" href="/images/favicon.png" />
+            </Head>
+            <Hero name={repo.data.title} />
+            <SpecialProducts title={repo.data.title} filter={repo.data.slug} displayFilter={true} />
+        </>;
+}
